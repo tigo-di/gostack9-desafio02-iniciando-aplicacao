f335ba9f91711bd285e7333b99970d9ac051e71a
Tiago Dias <tigo@webdeleve.net>
2019-11-22 05:43:56 -0300

    feat: user authentication

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,17 +1,42 @@
+import jwt from 'jsonwebtoken'; // module import precedes model import
+
 import User from '../models/User';
+
+import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
     // I get the body's email and password
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } }); // short syntax of { email: email }
+    const user = await User.findOne({ where: { email } }); // short syntax of { email: email } - The name and value of the variable are the same
 
     if (!user) {
       return res.status(401).json({ error: `User ${email} not found` });
     }
 
-    return res.status(200).json({ message: 'User found' });
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password is wrong. Try again' });
+    }
+
+    const { id, name } = user;
+
+    return res.status(200).json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      /*
+      method sign
+      - first param = payload
+      - second = text unique
+      - third - params extra
+      */
+      token: jwt.sign({ id }, authConfig.secret, {
+        expiresIn: authConfig.expiresIn,
+      }),
+    });
 
     /*
     return res.json({
---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  // variables to method sign of jwt to generate the token
+  secret: 'dbb9264f52507e5bde6f55d2ddc8f708',
+  expiresIn: '7d',
+};
