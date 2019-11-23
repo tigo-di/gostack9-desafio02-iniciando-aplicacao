b86573c4f34b763e7b143aa672dcca8a56eb6a06
Tiago Dias <tigo@webdeleve.net>
2019-11-23 03:21:15 -0300

    feat: user model base

---
+++ src/app/models/User.js
@@ -0,0 +1,26 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // exists only in code
+        password_hash: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash); // this method return true if password matchs this.passord_hash
+  }
+}
+
+export default User;
