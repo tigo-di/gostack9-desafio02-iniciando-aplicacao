000fbe24c767fb7e58f67453424469bf396a44a0
Tiago Dias <tigo@webdeleve.net>
2019-11-24 02:20:23 -0300

    feat: StudentController create user with yup validation

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -1,12 +1,44 @@
+import * as Yup from 'yup'; // schema validation
 import Student from '../models/Student';
 
 class StudentController {
   async store(req, res) {
+    // method object because req is a object
+    // shape defines format
+
+    const schema = Yup.object().shape({
+      name: Yup.string()
+        .trim()
+        .required(),
+      email: Yup.string()
+        .trim()
+        .email()
+        .required(),
+      age: Yup.number()
+        .positive()
+        .integer()
+        .required(),
+      height: Yup.number()
+        .positive()
+        .required(),
+      weight: Yup.number()
+        .positive()
+        .required(),
+    });
+
+    // return res.status(200).json({ message: 'Validation ok' });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     // Save data
     const { name, email, age, height, weight } = await Student.create(req.body);
 
+    // const { name, email, age, height, weight } = req.body;
+
     return res
-      .status('200')
+      .status(200)
       .json({ message: `${name} ${email} ${age} ${height}  ${weight} ` });
   }
 }
