279fafc009fd72ae041db02391371c7250e59aec
Tiago Dias <tigo@webdeleve.net>
2019-12-03 06:01:26 -0300

    feat: middleware auth.js + global route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
@@ -13,6 +14,9 @@ const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 routes.post('/students', StudentController.store);
+
+// global middleware
+routes.use(authMiddleware);
 
 routes.put('/students/:id', StudentController.update);
 
